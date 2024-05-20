335358c3af228443a165d360d69e6c5b3be9dba1
Viktor Shvetcov <vikshv@yandex.ru>
2024-05-20 13:14:45 +0300

    fix http to https

--- src/components/pages/events/eventFormPage/index.js
+++ src/components/pages/events/eventFormPage/index.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import angularMessages from 'angular-messages';
-import 'angular-ckeditor/angular-ckeditor.min.js';
+import '../../../../../bower_components/angular-ckeditor/angular-ckeditor.min.js';
 import component from './component';
 
 export default angular.module('app.components.pages.events.event.form', [
